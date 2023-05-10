b3cbbeea5aaa6b3ec03fc0f9948247c19c95e0c0
Vil_Blass <jeffry16snake@hotmail.com>
2023-05-10 14:07:58 +0200

    fuera alert

--- log_up.js
+++ log_up.js
@@ -49,222 +49,226 @@ function log_up() {
         body: JSON.stringify(data),
         headers: {
             'Content-Type': 'application/json'
+            
         }
 
         //Recibimos la respuesta de la base de datos
     }).then(res => res.json())
+
         .catch(error => console.error('Error:', error))
+
         .then(response => console.log('Success:', response));
+
 
 
     // //Comprobamos que todos los campos esten rellenos
     // if (nombre == "" || apellidos == "" || dni == "" || direccion == "" || telefono == "" || email == "" || password == "" || password2 == "") {
-    //     alert("Rellene todos los campos");
+    //     // alert("Rellene todos los campos");
     //     return;
     // }
 
     //Comprobamos que el telefono tenga 9 digitos
     if (telefono.length != 9) {
-        alert("El telefono debe tener 9 digitos");
+        // // alert("El telefono debe tener 9 digitos");
         return console.log(telefono);
-    }
+    }   
 
 
     //Comprobamos que el DNI tenga 9 caracteres
     if (dni.length != 9) {
-        alert("El DNI debe tener 9 caracteres");
+        // // alert("El DNI debe tener 9 caracteres");
         return;
     }
 
     //Comprobamos que el email tenga el formato correcto
     if (!email.includes("@")) {
-        alert("El email debe tener el formato correcto");
+        // // alert("El email debe tener el formato correcto");
         return;
     }
 
     //Comprobamos que el nombre tenga al menos 3 caracteres
     if (nombre.length < 3) {
-        alert("El nombre debe tener al menos 3 caracteres");
+        // // alert("El nombre debe tener al menos 3 caracteres");
         return;
     }
 
     //Comprobamos que el apellido tenga al menos 3 caracteres
     if (apellidos.length < 3) {
-        alert("El apellido debe tener al menos 3 caracteres");
+        // // alert("El apellido debe tener al menos 3 caracteres");
         return;
     }
 
     //Comprobamos que las contraseñas coincidan
     if (password != password2) {
-        alert("Las contraseñas no coinciden");
+        // alert("Las contraseñas no coinciden");
         return;
     }
 
     //Comprobamos que la contraseña tenga al menos 8 caracteres
     if (password.length < 8) {
-        alert("La contraseña debe tener al menos 8 caracteres");
+        // alert("La contraseña debe tener al menos 8 caracteres");
         return;
     }
 
     //Comprobamos que la contraseña tenga al menos una mayuscula
     if (password == password.toLowerCase()) {
-        alert("La contraseña debe tener al menos una mayuscula");
+        // alert("La contraseña debe tener al menos una mayuscula");
         return;
     }
 
     //Comprobamos que la contraseña tenga al menos una minuscula
     if (password == password.toUpperCase()) {
-        alert("La contraseña debe tener al menos una minuscula");
+        // alert("La contraseña debe tener al menos una minuscula");
         return;
     }
 
     //Comprobamos que la contraseña tenga al menos un numero
     if (password.search(/[0-9]/) < 0) {
-        alert("La contraseña debe tener al menos un numero");
+        // alert("La contraseña debe tener al menos un numero");
         return;
     }
 
     //Comprobamos que la contraseña tenga al menos un caracter especial
     if (password.search(/[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]/) < 0) {
-        alert("La contraseña debe tener al menos un caracter especial");
+        // alert("La contraseña debe tener al menos un caracter especial");
         return;
     }
 
     //Comprobamos que el DNI tenga el formato correcto
     if (dni.charAt(8) != "-" || dni.charAt(0) == "-" || dni.charAt(1) == "-" || dni.charAt(2) == "-" || dni.charAt(3) == "-" || dni.charAt(4) == "-" || dni.charAt(5) == "-" || dni.charAt(6) == "-" || dni.charAt(7) == "-" || dni.charAt(8) == "-" || dni.charAt(9) == "-") {
-        alert("El DNI debe tener el formato correcto");
+        // alert("El DNI debe tener el formato correcto");
         return;
     }
 
     //Comprobamos que el DNI tenga el formato correcto
     if (dni.charAt(0) != dni.charAt(0).toUpperCase() || dni.charAt(1) != dni.charAt(1).toUpperCase() || dni.charAt(2) != dni.charAt(2).toUpperCase()) {
-        alert("El DNI debe tener el formato correcto");
+        // alert("El DNI debe tener el formato correcto");
         return;
     }
 
     //Comprobamos que el DNI tenga el formato correcto
     if (dni.charAt(3) != dni.charAt(3).toLowerCase() || dni.charAt(4) != dni.charAt(4).toLowerCase() || dni.charAt(5) != dni.charAt(5).toLowerCase() || dni.charAt(6) != dni.charAt(6).toLowerCase() || dni.charAt(7) != dni.charAt(7).toLowerCase() || dni.charAt(8) != dni.charAt(8).toLowerCase() || dni.charAt(9) != dni.charAt(9).toLowerCase()) {
-        alert("El DNI debe tener el formato correcto");
+        // alert("El DNI debe tener el formato correcto");
         return;
     }
 
     //Comprobamos que el DNI tenga el formato correcto
     if (dni.charAt(0) != "X" && dni.charAt(0) != "Y" && dni.charAt(0) != "Z" && dni.charAt(0) != "x" && dni.charAt(0) != "y" && dni.charAt(0) != "z") {
-        alert("El DNI debe tener el formato correcto");
+        // alert("El DNI debe tener el formato correcto");
         return;
     }
 
     //Comprobamos que el DNI tenga el formato correcto
     if (dni.charAt(1) != dni.charAt(1).toUpperCase() || dni.charAt(2) != dni.charAt(2).toUpperCase()) {
-        alert("El DNI debe tener el formato correcto");
+        // alert("El DNI debe tener el formato correcto");
         return;
     }
 
     //Comprobamos que el DNI tenga el formato correcto
     if (dni.charAt(0) == "X" && dni.charAt(1) == "X" && dni.charAt(2) == "X") {
-        alert("El DNI debe tener el formato correcto");
+        // alert("El DNI debe tener el formato correcto");
         return;
     }
 
     //Comprobamos que el DNI tenga el formato correcto
     if (dni.charAt(0) == "Y" && dni.charAt(1) == "Y" && dni.charAt(2) == "Y") {
-        alert("El DNI debe tener el formato correcto");
+        // alert("El DNI debe tener el formato correcto");
         return;
     }
 
     //Comprobamos que el DNI tenga el formato correcto
     if (dni.charAt(0) == "Z" && dni.charAt(1) == "Z" && dni.charAt(2) == "Z") {
-        alert("El DNI debe tener el formato correcto");
+        // alert("El DNI debe tener el formato correcto");
         return;
     }
 
     //Comprobamos que el DNI tenga el formato correcto
     if (dni.charAt(0) == "x" && dni.charAt(1) == "x" && dni.charAt(2) == "x") {
-        alert("El DNI debe tener el formato correcto");
+        // alert("El DNI debe tener el formato correcto");
         return;
     }
 
     //Comprobamos que el DNI tenga el formato correcto
     if (dni.charAt(0) == "y" && dni.charAt(1) == "y" && dni.charAt(2) == "y") {
-        alert("El DNI debe tener el formato correcto");
+        // alert("El DNI debe tener el formato correcto");
         return;
     }
 
     //Comprobamos que el DNI tenga el formato correcto
     if (dni.charAt(0) == "z" && dni.charAt(1) == "z" && dni.charAt(2) == "z") {
-        alert("El DNI debe tener el formato correcto");
+        // alert("El DNI debe tener el formato correcto");
         return;
     }
 
     //Comprobamos que el DNI tenga 8 numeros
     if (dni.charAt(3) == "-" || dni.charAt(4) == "-" || dni.charAt(5) == "-" || dni.charAt(6) == "-" || dni.charAt(7) == "-" || dni.charAt(8) == "-" || dni.charAt(9) == "-" || dni.charAt(10) == "-") {
-        alert("El DNI debe tener 8 numeros");
+        // alert("El DNI debe tener 8 numeros");
         return;
     }
 
     //Comprobamos que el DNI tenga 8 numeros
     if (dni.charAt(3) == "0" && dni.charAt(4) == "0" && dni.charAt(5) == "0" && dni.charAt(6) == "0" && dni.charAt(7) == "0" && dni.charAt(8) == "0" && dni.charAt(9) == "0") {
-        alert("El DNI debe tener 8 numeros");
+        // alert("El DNI debe tener 8 numeros");
         return;
     }
 
     //Comprobamos que el DNI tenga 8 numeros
     if (dni.charAt(3) == "1" && dni.charAt(4) == "1" && dni.charAt(5) == "1" && dni.charAt(6) == "1" && dni.charAt(7) == "1" && dni.charAt(8) == "1" && dni.charAt(9) == "1") {
-        alert("El DNI debe tener 8 numeros");
+        // alert("El DNI debe tener 8 numeros");
         return;
     }
 
     //Comprobamos que el DNI tenga 8 numeros
     if (dni.charAt(3) == "2" && dni.charAt(4) == "2" && dni.charAt(5) == "2" && dni.charAt(6) == "2" && dni.charAt(7) == "2" && dni.charAt(8) == "2" && dni.charAt(9) == "2") {
-        alert("El DNI debe tener 8 numeros");
+        // alert("El DNI debe tener 8 numeros");
         return;
     }
 
     //Comprobamos que el DNI tenga 8 numeros
     if (dni.charAt(3) == "3" && dni.charAt(4) == "3" && dni.charAt(5) == "3" && dni.charAt(6) == "3" && dni.charAt(7) == "3" && dni.charAt(8) == "3" && dni.charAt(9) == "3") {
-        alert("El DNI debe tener 8 numeros");
+        // alert("El DNI debe tener 8 numeros");
         return;
     }
 
     //Comprobamos que el DNI tenga 8 numeros
     if (dni.charAt(3) == "4" && dni.charAt(4) == "4" && dni.charAt(5) == "4" && dni.charAt(6) == "4" && dni.charAt(7) == "4" && dni.charAt(8) == "4" && dni.charAt(9) == "4") {
-        alert("El DNI debe tener 8 numeros");
+        // alert("El DNI debe tener 8 numeros");
         return;
     }
 
     //Comprobamos que el DNI tenga 8 numeros
     if (dni.charAt(3) == "5" && dni.charAt(4) == "5" && dni.charAt(5) == "5" && dni.charAt(6) == "5" && dni.charAt(7) == "5" && dni.charAt(8) == "5" && dni.charAt(9) == "5") {
-        alert("El DNI debe tener 8 numeros");
+        // alert("El DNI debe tener 8 numeros");
         return;
     }
 
     //Comprobamos que el DNI tenga 8 numeros
     if (dni.charAt(3) == "6" && dni.charAt(4) == "6" && dni.charAt(5) == "6" && dni.charAt(6) == "6" && dni.charAt(7) == "6" && dni.charAt(8) == "6" && dni.charAt(9) == "6") {
-        alert("El DNI debe tener 8 numeros");
+        // alert("El DNI debe tener 8 numeros");
         return;
     }
 
     //Comprobamos que el DNI tenga 8 numeros
     if (dni.charAt(3) == "7" && dni.charAt(4) == "7" && dni.charAt(5) == "7" && dni.charAt(6) == "7" && dni.charAt(7) == "7" && dni.charAt(8) == "7" && dni.charAt(9) == "7") {
-        alert("El DNI debe tener 8 numeros");
+        // alert("El DNI debe tener 8 numeros");
         return;
     }
 
     //Comprobamos que el DNI tenga 8 numeros
     if (dni.charAt(3) == "8" && dni.charAt(4) == "8" && dni.charAt(5) == "8" && dni.charAt(6) == "8" && dni.charAt(7) == "8" && dni.charAt(8) == "8" && dni.charAt(9) == "8") {
-        alert("El DNI debe tener 8 numeros");
+        // alert("El DNI debe tener 8 numeros");
         return;
     }
 
     //Comprobamos que el DNI tenga 8 numeros
     if (dni.charAt(3) == "9" && dni.charAt(4) == "9" && dni.charAt(5) == "9" && dni.charAt(6) == "9" && dni.charAt(7) == "9" && dni.charAt(8) == "9" && dni.charAt(9) == "9") {
-        alert("El DNI debe tener 8 numeros");
+        // alert("El DNI debe tener 8 numeros");
         return;
     }
 
     //Comprobamos que el DNI tenga 8 numeros
     if (dni.charAt(3) == "0" && dni.charAt(4) == "0" && dni.charAt(5) == "0" && dni.charAt(6) == "0" && dni.charAt(7) == "0" && dni.charAt(8) == "0" && dni.charAt(9) == "0") {
-        alert("El DNI debe tener 8 numeros");
+        // alert("El DNI debe tener 8 numeros");
         return;
     }
 
@@ -282,7 +286,7 @@ function log_up() {
         dni.charAt(10) == "Q" || dni.charAt(10) == "R" || dni.charAt(10) == "S" || dni.charAt(10) == "T" ||
         dni.charAt(10) == "U" || dni.charAt(10) == "V" || dni.charAt(10) == "W" || dni.charAt(10) == "X" ||
         dni.charAt(10) == "Y" || dni.charAt(10) == "Z") {
-        alert("El DNI debe tener 1 letra");
+        // alert("El DNI debe tener 1 letra");
         return;
     }
 
@@ -300,13 +304,13 @@ function log_up() {
         || dni.charAt(10) == "u" || dni.charAt(10) == "v" ||
         dni.charAt(10) == "w" || dni.charAt(10) == "x"
         || dni.charAt(10) == "y" || dni.charAt(10) == "z") {
-        alert("El DNI debe tener 1 letra");
+        // alert("El DNI debe tener 1 letra");
         return;
     }
 
     //Comprobamos que el DNI tenga 1 letra 
     if (dni.charAt(10) == "ñ" || dni.charAt(10) == "Ñ") {
-        alert("El DNI debe tener 1 letra");
+        // alert("El DNI debe tener 1 letra");
         return;
     }
 
@@ -316,7 +320,7 @@ function log_up() {
         dni.charAt(10) == "5" || dni.charAt(10) == "6" ||
         dni.charAt(10) == "7" || dni.charAt(10) == "8" ||
         dni.charAt(10) == "9" || dni.charAt(10) == "0") {
-        alert("El DNI debe tener 1 letra");
+        // alert("El DNI debe tener 1 letra");
         return;
     }
 
@@ -334,7 +338,7 @@ function log_up() {
         || dni.charAt(10) == "´" || dni.charAt(10) == "ç" || dni.charAt(10) == "Ç" || dni.charAt(10) == "´"
         || dni.charAt(10) == "~" || dni.charAt(10) == "´" || dni.charAt(10) == "ç" || dni.charAt(10) == "Ç"
         || dni.charAt(10) == "´" || dni.charAt(10) == "~") {
-        alert("El DNI debe tener 1 letra");
+        // alert("El DNI debe tener 1 letra");
         return;
     }
 
@@ -347,7 +351,7 @@ function log_up() {
         || dni.charAt(10) == "´" || dni.charAt(10) == "ç" || dni.charAt(10) == "Ç" || dni.charAt(10) == "´"
         || dni.charAt(10) == "~" || dni.charAt(10) == "´" || dni.charAt(10) == "ç" || dni.charAt(10) == "Ç"
         || dni.charAt(10) == "´" || dni.charAt(10) == "~") {
-        alert("El DNI debe tener 1 letra");
+        // alert("El DNI debe tener 1 letra");
         return;
     }
 
@@ -358,7 +362,7 @@ function log_up() {
         || dni.charAt(10) == "´" || dni.charAt(10) == "~" || dni.charAt(10) == "´" || dni.charAt(10) == "ç"
         || dni.charAt(10) == "Ç" || dni.charAt(10) == "´" || dni.charAt(10) == "~" || dni.charAt(10) == "´"
         || dni.charAt(10) == "ç" || dni.charAt(10) == "Ç" || dni.charAt(10) == "´" || dni.charAt(10) == "~") {
-        alert("El DNI debe tener 1 letra");
+        // alert("El DNI debe tener 1 letra");
         return;
     }
 
@@ -370,10 +374,10 @@ function log_up() {
         || dni.charAt(10) == "´" || dni.charAt(10) == "~" || dni.charAt(10) == "´" || dni.charAt(10) == "ç"
         || dni.charAt(10) == "Ç" || dni.charAt(10) == "´" || dni.charAt(10) == "~" || dni.charAt(10) == "´"
         || dni.charAt(10) == "ç" || dni.charAt(10) == "Ç" || dni.charAt(10) == "´" || dni.charAt(10) == "~") {
-        alert("El DNI debe tener 1 letra"); //Mensaje de error
+        // alert("El DNI debe tener 1 letra"); //Mensaje de error
         return;
     } else { //Si el DNI es correcto, se muestra un mensaje de confirmacion
-        alert("DNI correcto");
+        // alert("DNI correcto");
         //Mensaje de confirmacion
 
 
@@ -389,7 +393,7 @@ function log_up() {
         dni.charAt(10) == "´" || dni.charAt(10) == "~" || dni.charAt(10) == "´" || dni.charAt(10) == "ç" ||
         dni.charAt(10) == "Ç" || dni.charAt(10) == "´" || dni.charAt(10) == "~" || dni.charAt(10) == "´" ||
         dni.charAt(10) == "ç" || dni.charAt(10) == "Ç" || dni.charAt(10) == "´" || dni.charAt(10) == "~") {
-        alert("El DNI debe tener 1 letra"); //Mensaje de error
+        // alert("El DNI debe tener 1 letra"); //Mensaje de error
         return;
     }
 
@@ -453,8 +457,16 @@ function log_up() {
     }
 
     // si los elemntos son validos pasar a la pagina principal
-    if (nombre.value != "" && apellidos.value != "" && dni.value != "" && email.value != "" && telefono.value != "" && direccion.value != "" && password.value != "" && password2.value != "") {
+
+    if (nombre.value != "" && apellidos.value != "" && dni.value != "" &&
+     email.value != "" && telefono.value != "" && direccion.value != "" &&
+      password.value != "" && password2.value != "") {
         location.href = "index.html";
+    } else {
+        // alert("Rellene todos los campos");
     }
+
+    // si los elemntos son validos pasar a la pagina principal
+ 
 }
 
