7706f8a5ed19951fb347988552a88e5589cd5387
yusra <yousra.rhazouali@gmail.com>
2023-05-11 13:57:03 +0200

    contacto yus

--- yus.js
+++ yus.js
@@ -1,14 +1,6 @@
+const darkButton = document.querySelector(".Dark input");
 
 let hinicio=document.getElementById("tinicio")
 let hfinale=document.getElementById("tfinal")
 
 document.getElementById("btn").addEventListener("click", validar);
-
-
-function validar() {
-    if{
-        
-    }
-    
-  document.getElementById("demo").innerHTML="disponible"
-}
